db8e7cadd49e58b561b87630b7749351cc200ebc
Jérémy <jeremy.pro.lp@gmail.com>
2022-01-19 19:06:45 +0100

    fix keys issue in DataTable

--- components/data/DataTable.jsx
+++ components/data/DataTable.jsx
@@ -12,48 +12,61 @@ function DataTable({ columns, data }) {
           {...getTableProps()}
           className="w-full border-collapse table-auto"
         >
-          <thead className="">
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                    className="text-xs text-left p-3 uppercase sticky top-0 whitespace-nowrap bg-light-light dark:bg-dark-dark"
-                  >
-                    {column.render('Header')}
-                    <span>
-                      {column.isSorted ? (
-                        column.isSortedDesc ? (
-                          <FiChevronUp
-                            aria-label="sorted descending"
-                            className="inline-block"
-                          />
-                        ) : (
-                          <FiChevronDown
-                            aria-label="sorted ascending"
-                            className="inline-block"
-                          />
-                        )
-                      ) : null}
-                    </span>
-                  </th>
-                ))}
-              </tr>
-            ))}
+          <thead>
+            {headerGroups.map(headerGroup => {
+              const { key, ...restHeaderGroupProps } =
+                headerGroup.getHeaderGroupProps()
+              return (
+                <tr key={key} {...restHeaderGroupProps}>
+                  {headerGroup.headers.map(column => {
+                    const { key, ...restColumn } = column.getHeaderProps()
+                    return (
+                      <th
+                        key={key}
+                        {...restColumn}
+                        className="text-xs text-left p-3 uppercase sticky top-0 whitespace-nowrap bg-light-light dark:bg-dark-dark"
+                      >
+                        {column.render('Header')}
+                        <span>
+                          {column.isSorted ? (
+                            column.isSortedDesc ? (
+                              <FiChevronUp
+                                aria-label="sorted descending"
+                                className="inline-block"
+                              />
+                            ) : (
+                              <FiChevronDown
+                                aria-label="sorted ascending"
+                                className="inline-block"
+                              />
+                            )
+                          ) : null}
+                        </span>
+                      </th>
+                    )
+                  })}
+                </tr>
+              )
+            })}
           </thead>
-          <tbody {...getTableBodyProps()} className="divide-y">
+          <tbody {...getTableBodyProps} className="divide-y">
             {rows.map(row => {
               prepareRow(row)
+              const { key, ...restRowProps } = row.getRowProps()
               return (
-                <tr {...row.getRowProps()} className="">
-                  {row.cells.map(cell => (
-                    <td
-                      {...cell.getCellProps()}
-                      className="text-left py-6 px-3 text-sm whitespace-nowrap"
-                    >
-                      {cell.render('Cell')}
-                    </td>
-                  ))}
+                <tr key={key} {...restRowProps}>
+                  {row.cells.map(cell => {
+                    const { key, ...restCellProps } = cell.getCellProps()
+                    return (
+                      <td
+                        key={key}
+                        {...restCellProps}
+                        className="text-left py-6 px-3 text-sm whitespace-nowrap"
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
                 </tr>
               )
             })}
